fa311146daa54a225c7b629f5c515a41bdec78df
KUNAL PATEL <pKunal826@gmail.com>
2022-10-11 22:17:00 +0530

    Initial commit

---
+++ app/src/main/java/kunal/project/productworld/introduction2.java
@@ -0,0 +1,14 @@
+package kunal.project.productworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class introduction2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction2);
+    }
+}
